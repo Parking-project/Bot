c9275f272acf02ffa914c9449967cd116c9b7b4e
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-05-02 02:37:29 +0300

    api_token_requests

--- core/requests/token_requests.py
+++ core/requests/token_requests.py
@@ -3,14 +3,13 @@ from .base_requests import send_get_request, send_post_request
 class TokenController:
     @classmethod
     def login(cls, login: str, password: str):
-        tokens = send_post_request(
+        return send_post_request(
             "/token/login",
             json={
                 "login": login,
                 "password": password
             }
-        ).json()["tokens"]
-        return tokens["access"], tokens["refresh"]
+        )
     
     @classmethod
     def register(cls, login: str, password: str, display_name: str):
@@ -32,17 +31,23 @@ class TokenController:
         )
     
     @classmethod
-    def check(cls, token):
-        return send_get_request(
+    def check(cls, access, refresh):
+        response = send_get_request(
             "/token/check",
             json={},
-            token=token
+            token=access
         )
+        if response.status_code == 200:
+            return access, refresh
+        response = cls.refresh(refresh_token=refresh)
+        if response.status_code == 200:
+            return response.json()["access"], refresh
+        return None, None
     
     @classmethod
     def logout(cls, token):
         return send_get_request(
             "/token/logout",
-            json={},
+            data={},
             token=token
         )
