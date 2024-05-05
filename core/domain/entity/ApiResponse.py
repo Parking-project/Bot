7d6029dbe2907ec8da37e1460837f2e693d1ade7
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-05-05 20:12:49 +0300

    ApiResponse_print_exception

--- core/domain/entity/ApiResponse.py
+++ core/domain/entity/ApiResponse.py
@@ -4,6 +4,8 @@ class ApiResponse:
     def __init__(self, data, is_exception=False):
         self.data = data
         self.is_exception = is_exception
+        if is_exception:
+            print("\n\n\nData = ", data, "\n\n\n")
 
     def IsException(self):
         return self.is_exception
