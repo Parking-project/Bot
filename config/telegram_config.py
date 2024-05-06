32f04ad15247041d4516b954c15433e91ef4af42
Дмитрий Сорокин <isip_d.m.sorokin@mpt.ru>
2024-05-06 09:08:03 +0300

    telegram_config_little_change

--- config/telegram_config.py
+++ config/telegram_config.py
@@ -6,3 +6,5 @@ load_dotenv()
 
 class TelegramConfig:
     TOKEN_API: str = os.getenv('TOKEN_API')
+    GROUP_ID: int = os.getenv('GROUP_ID')
+    RESERVETION_GROUP_ID: int = os.getenv('RESERVETION_GROUP_ID')
